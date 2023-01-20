b6eda90b06a6b3af5c1703c6b2a67dc472046fb4
Lucas Neves <lucasdeoliveiraneves@Mac-mini-de-Lucas.local>
2023-01-20 00:29:25 -0300

    Interpolation syntax

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { RoomsComponent } from './rooms/rooms.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    RoomsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/rooms/rooms.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rooms',
+  templateUrl: './rooms.component.html',
+  styleUrls: ['./rooms.component.sass']
+})
+export class RoomsComponent {
+  hotelName = "Hilton Hotel"
+}
