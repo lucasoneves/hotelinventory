44b59177f0cbe42045a279515138f6934e950e1c
Lucas Neves <lucasdeoliveiraneves@Mac-mini-de-Lucas.local>
2023-01-21 12:34:28 -0300

    Ng ig directive

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,22 +1,28 @@
 import { Component } from '@angular/core';
-import { Room } from './rooms';
+import { Room, RoomsList } from './rooms';
 
 @Component({
   selector: 'app-rooms',
   templateUrl: './rooms.component.html',
-  styleUrls: ['./rooms.component.sass']
+  styleUrls: ['./rooms.component.sass'],
 })
 export class RoomsComponent {
-  hotelName = "Hilton Hotel";
+  hotelName = 'Hilton Hotel';
   numberOfRooms = 20;
   hideRooms = false;
   toggle() {
-    this.hideRooms = !this.hideRooms
+    this.hideRooms = !this.hideRooms;
   }
   rooms: Room = {
     id: 1,
     totalRooms: 20,
     available: 10,
-    bookedRooms: 10
-  }
+    bookedRooms: 10,
+  };
+
+  roomList: RoomsList[] = [
+    { roomType: 'Deluxe', amenities: 'Air cond', price: 40, photos: '', checkinTime: new Date() },
+    { roomType: 'Premium', amenities: 'Air cond', price: 20, photos: '', checkinTime: new Date() },
+    { roomType: 'Basic', amenities: 'Air cond', price: 10, photos: '', checkinTime: new Date() }
+  ];
 }
